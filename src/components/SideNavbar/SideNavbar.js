89ada166d6d7381cf2c2cfca8b6899df8dbdd66d
Eduard <hayrapetyan-edo@inbox.ru>
2019-01-10 19:01:05 +0400

    First Commit

---
+++ src/components/SideNavbar/SideNavbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+import { Group, GroupHeader, Navbar, NavItem } from './styled';
+import logo from '../../images/Spotify_Icon_RGB_White.png';
+
+class SideNavbar extends Component {
+  render() {
+    return (
+      <Navbar>
+        <GroupHeader>
+          <img src={logo} alt="spotify logo" />
+        </GroupHeader>
+   
+        <Group>
+          <NavItem to="/browse">Home</NavItem>
+        </Group>
+      </Navbar>
+    );
+  }
+}
+
+export default SideNavbar;
